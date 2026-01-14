cca9a3813bbaf51041fd406c7a12f17a9e589e8c
firstofdev <hdralonzy@gmail.com>
2026-01-14 18:58:39 +0300

    Add files via upload

---
+++ dbs15162823.sql
@@ -0,0 +1,500 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.11
+-- https://www.phpmyadmin.net/
+--
+-- Host: db5019378605.hosting-data.io
+-- Generation Time: Jan 14, 2026 at 03:57 PM
+-- Server version: 8.0.36
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbs15162823`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contracts`
+--
+
+CREATE TABLE `contracts` (
+  `id` int NOT NULL,
+  `tenant_id` int NOT NULL,
+  `unit_id` int NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `total_amount` decimal(15,2) NOT NULL,
+  `paid_amount` decimal(15,2) DEFAULT '0.00',
+  `status` enum('active','expired') DEFAULT 'active',
+  `signature_img` longtext
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `contracts`
+--
+
+INSERT INTO `contracts` (`id`, `tenant_id`, `unit_id`, `start_date`, `end_date`, `total_amount`, `paid_amount`, `status`, `signature_img`) VALUES
+(2, 3, 2, '2025-02-13', '2026-02-01', '120000.00', '0.00', 'active', NULL),
+(3, 4, 3, '2025-04-13', '2026-04-13', '120000.00', '0.00', 'active', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inspection_photos`
+--
+
+CREATE TABLE `inspection_photos` (
+  `id` int NOT NULL,
+  `contract_id` int NOT NULL,
+  `photo_type` enum('check_in','check_out') NOT NULL,
+  `photo_path` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `maintenance`
+--
+
+CREATE TABLE `maintenance` (
+  `id` int NOT NULL,
+  `property_id` int DEFAULT NULL,
+  `unit_id` int DEFAULT NULL,
+  `vendor_id` int DEFAULT NULL,
+  `description` text COLLATE utf8mb4_general_ci,
+  `cost` decimal(15,2) DEFAULT NULL,
+  `status` enum('pending','completed','paid') COLLATE utf8mb4_general_ci DEFAULT 'pending',
+  `request_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meter_readings`
+--
+
+CREATE TABLE `meter_readings` (
+  `id` int NOT NULL,
+  `contract_id` int DEFAULT NULL,
+  `unit_id` int DEFAULT NULL,
+  `reading_type` enum('check_in','check_out','periodic') COLLATE utf8mb4_general_ci DEFAULT 'periodic',
+  `elec_reading` decimal(12,2) DEFAULT NULL,
+  `water_reading` decimal(12,2) DEFAULT NULL,
+  `reading_date` date DEFAULT NULL,
+  `notes` text COLLATE utf8mb4_general_ci,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `id` int NOT NULL,
+  `uuid` varchar(64) DEFAULT NULL,
+  `contract_id` int NOT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `amount` decimal(15,2) NOT NULL,
+  `payment_method` varchar(30) DEFAULT NULL,
+  `note` text,
+  `due_date` date NOT NULL,
+  `paid_date` date DEFAULT NULL,
+  `status` enum('pending','paid') DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `properties`
+--
+
+CREATE TABLE `properties` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` text,
+  `manager` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `properties`
+--
+
+INSERT INTO `properties` (`id`, `name`, `address`, `manager`, `phone`) VALUES
+(4, 'واجهة البرج', 'البرج', 'سلطان', '0505256365');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `k` varchar(50) NOT NULL,
+  `v` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`k`, `v`) VALUES
+('address', ''),
+('admin_whatsapp', ''),
+('alert_days', '30'),
+('alerts_digest', 'weekly'),
+('auto_backup', 'weekly'),
+('backup_frequency', '8'),
+('company_name', 'دار  الميار للمقاولات'),
+('cr_no', ''),
+('currency', 'SAR'),
+('currency_code', 'ر.س'),
+('date_format', 'Y-m-d'),
+('default_payment_method', 'bank_transfer'),
+('email', ''),
+('invoice_grace_days', '5'),
+('invoice_prefix', 'INV-'),
+('invoice_terms', ''),
+('logo', 'uploads/69667fd9c7aa83.46742256.png'),
+('maintenance_message', 'النظام تحت صيانة مجدولة، قد تتأخر بعض الخدمات.'),
+('maintenance_mode', 'off'),
+('ocr_api_key', ''),
+('ocr_api_url', ''),
+('overdue_threshold', '5'),
+('payment_portal_url', ''),
+('phone', ''),
+('reporting_email', ''),
+('smart_features_mode', 'force'),
+('support_email', ''),
+('support_phone', ''),
+('target_collection', '95'),
+('target_occupancy', '90'),
+('tenant_portal_url', ''),
+('timezone', 'Asia/Riyadh'),
+('vat_no', ''),
+('vat_percent', '15'),
+('whatsapp_api_url', ''),
+('whatsapp_number', ''),
+('whatsapp_token', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tenants`
+--
+
+CREATE TABLE `tenants` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `id_number` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tenants`
+--
+
+INSERT INTO `tenants` (`id`, `name`, `phone`, `id_number`, `email`) VALUES
+(3, 'سمح', '0505256365', '1030862203', ''),
+(4, 'سدرة', '0555555212', '1030862203', 'ALJENTEL1111@HOTMAIL.COM'),
+(5, 'باو', '05205254121', '1030286853', 'ALJENTEL1111@HOTMAIL.COM');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `id` int NOT NULL,
+  `payment_id` int DEFAULT NULL,
+  `amount_paid` decimal(15,2) DEFAULT NULL,
+  `payment_method` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `transaction_date` date DEFAULT NULL,
+  `notes` text COLLATE utf8mb4_general_ci,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `units`
+--
+
+CREATE TABLE `units` (
+  `id` int NOT NULL,
+  `property_id` int NOT NULL,
+  `unit_name` varchar(100) NOT NULL,
+  `type` varchar(50) DEFAULT 'apartment',
+  `yearly_price` decimal(15,2) DEFAULT '0.00',
+  `elec_meter_no` varchar(50) DEFAULT NULL,
+  `water_meter_no` varchar(50) DEFAULT NULL,
+  `status` enum('available','rented') DEFAULT 'available'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `units`
+--
+
+INSERT INTO `units` (`id`, `property_id`, `unit_name`, `type`, `yearly_price`, `elec_meter_no`, `water_meter_no`, `status`) VALUES
+(2, 4, 'سمح', 'shop', '120000.00', '0', '0', 'rented'),
+(3, 4, 'سدرة', 'shop', '120000.00', '0', '0', 'rented'),
+(4, 4, 'باو', 'shop', '120000.00', '0', '0', 'available');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `role` enum('admin','staff') DEFAULT 'staff'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `full_name`, `role`) VALUES
+(1, 'admin101', '$2y$12$bGZHfyn/8GYa.BDEQdwHr.qFIhPFFAjCBcuBKye8gtww.UB2e27Q6', 'المدير العام', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vendors`
+--
+
+CREATE TABLE `vendors` (
+  `id` int NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `service_type` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `phone` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contracts`
+--
+ALTER TABLE `contracts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tenant_id` (`tenant_id`),
+  ADD KEY `unit_id` (`unit_id`);
+
+--
+-- Indexes for table `inspection_photos`
+--
+ALTER TABLE `inspection_photos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `contract_id` (`contract_id`);
+
+--
+-- Indexes for table `maintenance`
+--
+ALTER TABLE `maintenance`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `property_id` (`property_id`);
+
+--
+-- Indexes for table `meter_readings`
+--
+ALTER TABLE `meter_readings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `contract_id` (`contract_id`),
+  ADD KEY `unit_id` (`unit_id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `contract_id` (`contract_id`);
+
+--
+-- Indexes for table `properties`
+--
+ALTER TABLE `properties`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`k`);
+
+--
+-- Indexes for table `tenants`
+--
+ALTER TABLE `tenants`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `payment_id` (`payment_id`);
+
+--
+-- Indexes for table `units`
+--
+ALTER TABLE `units`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `property_id` (`property_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `vendors`
+--
+ALTER TABLE `vendors`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contracts`
+--
+ALTER TABLE `contracts`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `inspection_photos`
+--
+ALTER TABLE `inspection_photos`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `maintenance`
+--
+ALTER TABLE `maintenance`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `meter_readings`
+--
+ALTER TABLE `meter_readings`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `properties`
+--
+ALTER TABLE `properties`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tenants`
+--
+ALTER TABLE `tenants`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `transactions`
+--
+ALTER TABLE `transactions`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `units`
+--
+ALTER TABLE `units`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `vendors`
+--
+ALTER TABLE `vendors`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `contracts`
+--
+ALTER TABLE `contracts`
+  ADD CONSTRAINT `contracts_ibfk_1` FOREIGN KEY (`tenant_id`) REFERENCES `tenants` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `contracts_ibfk_2` FOREIGN KEY (`unit_id`) REFERENCES `units` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `inspection_photos`
+--
+ALTER TABLE `inspection_photos`
+  ADD CONSTRAINT `inspection_photos_ibfk_1` FOREIGN KEY (`contract_id`) REFERENCES `contracts` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `maintenance`
+--
+ALTER TABLE `maintenance`
+  ADD CONSTRAINT `maintenance_ibfk_1` FOREIGN KEY (`property_id`) REFERENCES `properties` (`id`);
+
+--
+-- Constraints for table `meter_readings`
+--
+ALTER TABLE `meter_readings`
+  ADD CONSTRAINT `meter_readings_ibfk_1` FOREIGN KEY (`contract_id`) REFERENCES `contracts` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `meter_readings_ibfk_2` FOREIGN KEY (`unit_id`) REFERENCES `units` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`contract_id`) REFERENCES `contracts` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`payment_id`) REFERENCES `payments` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `units`
+--
+ALTER TABLE `units`
+  ADD CONSTRAINT `units_ibfk_1` FOREIGN KEY (`property_id`) REFERENCES `properties` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
